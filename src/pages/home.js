9388d2fa14e01d2b23be09c2d65fa0a6646a2c81
Damian Soria <damian.soria@youbim.com>
2020-04-29 16:11:05 -0300

    feat: add new component searchBar and Navbar

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Layout from '../components/Layout';
+import NavBar from '../components/NavBar';
 
 import '../styles/home.scss';
 
 const Home = ({suggestions}) => {
 
-    console.log(suggestions);
+    const [text, setText] = useState('');
 
+    function onChangeText(textVal) {
+        setText(textVal);
+    }
+
+    function onChangeSelection(textVal) {
+
+    }
+
+    
     return (
         <Layout title="Home">
+            <NavBar 
+                text={text}
+                suggestions={suggestions}
+                onChangeText={onChangeText}
+                onChangeSelection={onChangeSelection}
+            />
             <div className="main-container">
                 {suggestions}
             </div>
